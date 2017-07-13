22e91e9ef889efb9c0def3223765706ea83b7840
Rory <srsullivan01@gmail.com>
2017-07-13 16:36:32 -0400

    fixed onclick functionality

--- public/app.js
+++ public/app.js
@@ -1,3 +1,14 @@
+$(document).ready(function(){
+    $('#startButton').click(function(){
+       start();
+    });
+    $('#hitButton').click(function(){
+      hit();
+    });
+    $('#standButton').click(function(){
+      stand();
+    });
+  });
 //build a deck
 //tried one big array, doesn't work that well, make arrays for suites + values then push to a deck
 var suites = ["clubs", "spades", "diamonds", "hearts"];
@@ -22,9 +33,6 @@ function start(){
     }
   }
 }
-
-
-
 //shfufle the deck you just made
   //make two positions and then move cards based on those
 function shuffle(){
@@ -116,12 +124,13 @@ function getCardInterface(card){
   var cardDiv = document.createElement('div');
   cardDiv.className = 'card';
   cardDiv.innerHTML = card.Suites + " " + card.Value;
-  return cardDiv;
   console.log("getCardInterface is running");
+  return cardDiv;
 }
 
 //Hit button
 var currentPlayer = 0;
+
 function hit(){
   //take a card from deck and pop to player
   var card = deck.pop();
@@ -143,7 +152,7 @@ function checkScore() {
 //write stay function
   //find current player, remove them from 'active' and make next player active.
   //once that has been done, check score and end game if player's score is in the right range
-function stay(){
+function stand(){
   if(currentPlayer != players.length - 1){
     document.getElementById('player ' + currentPlayer).classList.remove('active');
     currentPlayer += 1;
@@ -169,356 +178,9 @@ function stop(){
   document.getElementById('status').innerHTML = "Winner: Player " + players[winner].ID;
   console.log("stop function is working");
 }
+
 //issues:
-  //nothing happens when i click restart
+  //nothing happens when i click restart--SOLVED
   //there isnt a start fiunction
   //everything is tied to some othr function so nothing is happening
-/*
-//convert the dealt cards to values that make sense within this context
-var getValue = function(card) {
-  if(card % 13 === 0 || card % 13 === 11 || card % 13 === 12) {
-        return 10;
-    }
-    else if (card % 13 === 1) {
-    return 11;
-}
-    else {
-        return card % 13;
-    }
-};
-
-//score what player has been dealt
-var score = function(){
-  console.log("score is running");
-    return (getValue(card1) + getValue(card2));
-};
-//move this to display in p
-console.log("You have cards " + card1 + " and " + card2 +
-        " for a score of " + score());
-
-
-var cardArray = [
-  {
-    value: 1,
-    suite: "clubs",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_ca.png,
-  },
-  {
-    value: 2,
-    suite: "clubs",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_c2.png,
-  },
-  {
-    value: 3,
-    suite: "clubs",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_c3.png,
-  },
-  {
-    value: 4,
-    suite: "clubs",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_c4.png,
-  },
-  {
-    value: 5,
-    suite: "clubs",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_c5.png,
-  },
-  {
-    value: 6,
-    suite: "clubs",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_c6.png,
-  },
-  {
-    value: 7,
-    suite: "clubs",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_c7.png,
-  },
-  {
-    value: 8,
-    suite: "clubs",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_c8.png,
-  },
-  {
-    value: 9,
-    suite: "clubs",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_c9.png,
-  },
-  {
-    value: 10,
-    suite: "clubs",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_ck.png,
-  },
-  {
-    value: 10,
-    suite: "clubs",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_cq.png,
-  },
-  {
-    value: 10,
-    suite: "clubs",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_cj.png,
-  },
-  {
-    value: 1,
-    suite: "diamonds",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_da.png,
-  },
-  {
-    value: 2,
-    suite: "diamonds",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_d2.png,
-  },
-  {
-    value: 3,
-    suite: "diamonds",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_d3.png,
-  },
-  {
-    value: 4,
-    suite: "diamonds",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_d4.png,
-  },
-  {
-    value: 5,
-    suite: "diamonds",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_d5.png,
-  },
-  {
-    value: 6,
-    suite: "diamonds",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_d6.png,
-  },
-  {
-    value: 7,
-    suite: "diamonds",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_d7.png,
-  },
-  {
-    value: 8,
-    suite: "diamonds",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_d8.png,
-  },
-  {
-    value: 9,
-    suite: "diamonds",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_d9.png,
-  },
-  {
-    value: 10,
-    suite: "diamonds",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_d10.png,
-  },
-  {
-    value: 10,
-    suite: "diamonds",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_dj.png,
-  },
-  {
-    value: 10,
-    suite: "diamonds",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_dk.png,
-  },
-  {
-    value: 10,
-    suite: "diamonds",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_dq.png,
-  },
-  {
-    value: 1,
-    suite: "hearts",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_ha.png,
-  },
-  {
-    value: 2,
-    suite: "hearts",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_h2.png,
-  },
-  {
-    value: 3,
-    suite: "hearts",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_h3.png,
-  },
-  {
-    value: 4,
-    suite: "hearts",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_h4.png,
-  },
-  {
-    value: 5,
-    suite: "hearts",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_h5.png,
-  },
-  {
-    value: 6,
-    suite: "hearts",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_h6.png,
-  },
-  {
-    value: 7,
-    suite: "hearts",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_h7.png,
-  },
-  {
-    value: 8,
-    suite: "hearts",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_h8.png,
-  },
-  {
-    value: 9,
-    suite: "hearts",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_h9.png,
-  },
-  {
-    value: 10,
-    suite: "hearts",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_h10.png,
-  },
-  {
-    value: 10,
-    suite: "hearts",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_hj.png,
-  },
-  {
-    value: 10,
-    suite: "hearts",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_hk.png,
-
-  },
-  {
-    value: 10,
-    suite: "hearts",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_hq.png,
-  },
-  {
-    value: 1,
-    suite: "spades",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_sa.png,
-  },
-  {
-    value: 2,
-    suite: "spades",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_s2.png,
-  },
-  {
-    value: 3,
-    suite: "spades",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_s3.png,
-  },
-  {
-    value: 4,
-    suite: "spades",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_s4.png,
-  },
-  {
-    value: 5,
-    suite: "spades",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_s5.png,
-  },
-  {
-    value: 6,
-    suite: "spades",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_s6.png,
-  },
-  {
-    value: 7,
-    suite: "spades",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_s7.png,
-  },
-  {
-    value: 8,
-    suite: "spades",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_s8.png,
-  },
-  {
-    value: 9,
-    suite: "spades",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_s9.png,
-  },
-  {
-    value: 10,
-    suite: "spades",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_s10.png,
-  },
-  {
-    value: 10,
-    suite: "spades",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_sj.png,
-  },
-  {
-    value: 10,
-    suite: "spades",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_sk.png,
-  },
-  {
-    value: 10,
-    suite: "spades",
-    src: public/images/poker_cards_chips_2d/PNGs/cards/Set_B/small/card_b_sq.png,
-  },
-];
-
-/*var blackJack = {
-  gameRunning: function(){
-    console.log('blackJack.gameRunning() is running');
-
-    if (blackJack.isRunning) {
-      console.log('blackjack is running');
-      //start with two cards dealt
-      //create function for hit
-        //deal two to dealer + remove those from the array
-        //deal two to player + remove those from the array
-      //create function for stand
-      //create reset function
-      blackJack.dealCards();
-      AppController.handleCards();
-    }
-  dealCards: function(){},
-  hit: function(){},
-  stand: function(){},
-  },
-*/
-
-
-
-/*GIVEN: the game has loaded and Im ready to play
-WHEN: I click the button labeled 'deal'
-THEN: the cards are dealed and I can begin the game */
-
-//need function to deal cards
-/*
-getCards: function() {
-  console.log("dealCards is running");
-  return [
-  //that function should choose two cards at random from the array,
-  cardArray[Math.floor(Math.random() * cardArray.length)],
-  cardArray[Math.floor(Math.random() * cardArray.length)],
-  ];
-
-  //and push cards into the player and dealer's hands
-}
-dealCards: function() {
-  var tmp = cardArray.slice(cardArray);
-  var ret = [];
-
-  for (var i = 0; i < cardArray.length; i++) {
-    var index = Math.floor(Math.random() * tmp.length);
-    var removed = tmp.splice(index, 2);
-
-    ret.push(removed[0]);
-  }
-  return ret;
-}
-console.log(ret);
-
-
-//tie that function to run on reset btton
-},
-$(function(){
-  // Attach AppController methods to the DOM as event handlers here.
-  $('#reset').on('click', function (event) {
-    event.dealCards();
-
-    console.log('click event is working');
-
-    AppController.handleClickStart();
-
-  }) */
+  //hit function doesnt
